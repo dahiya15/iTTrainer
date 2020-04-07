96d5ef070f0c4ad204c8526dc9f3d32fddcdb896
Harry <harrydahiya@gmail.com>
2020-04-07 17:37:30 +0100

    HelloTesting  program

---
+++ src/HelloTesting.java
@@ -0,0 +1,10 @@
+public class HelloTesting {
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Hi Team welcome to Automation Testing");
+
+	}
+
+}
